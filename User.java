74c4003aaf0606cd7805cc5457268dcb07bbff16
achyuthan2606 <129186574+achyuthan2606@users.noreply.github.com>
2023-06-12 13:56:54 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,41 @@
+package com.okayjava.html.model;
+
+public class User {
+	private String fullname;
+	private String email;
+	private String dob;
+	private String pass;
+	private String gender;
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	} 
+	
+
+}
